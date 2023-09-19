be992e9e51bd8f225b939a7e0f31d4f26a266a47
wachcio <wachcio@poczta.fm>
2023-09-19 15:47:56 +0200

    change subjects

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,50 @@ import axios from 'axios'; // Zaimportuj Axios
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [activeSubjectQuestions, setActiveSubjectQuestions] = useState([]);
+  const [activeSubjectIndex, setActiveSubjectIndex] = useState(0);
+  const [subjects, setSubjects] = useState([
+    'Radiotechnika',
+    'BHP',
+    'Procedury i zwyczaje operatorskie',
+    'Przepisy dotyczące radiokomunikacyjnej służby amatorskiej',
+  ]);
+
+  const changeSubject = (index) => {
+    setActiveSubjectIndex(index);
+  };
 
   useEffect(() => {
     // Użyj Axios do wczytania danych z pliku JSON
     axios
-      .get('http://wachcio.pl/questions.json')
+      .get('http://egazminkf.wachcio.pl/questions.json')
       .then((response) => {
-        setQuestions(response.data.BHP);
-        console.log(response.data);
+        setQuestions(response.data);
+        setActiveSubjectQuestions(response.data[subjects[activeSubjectIndex]]);
+        console.log(questions);
       })
       .catch((error) =>
         console.error('Błąd wczytywania danych z pliku JSON:', error)
       );
-  }, []);
+  }, [subjects, activeSubjectIndex]);
 
   return (
     <div className="bg-gray-100 min-h-screen p-8">
+      {subjects.map((subject, index) => (
+        <button
+          onClick={() => changeSubject(index)} // Popraw wywołanie funkcji changeSubject
+          className="mb-4 bg-white p-2 m-2 shadow rounded font-normal"
+          key={index}
+        >
+          {subject}
+        </button>
+      ))}
       <h1 className="text-3xl font-bold mb-4">Pytania i odpowiedzi</h1>
+      <h2 className="text-2xl font-bold mb-4">
+        {subjects[activeSubjectIndex]}
+      </h2>
       <ol className="list-decimal list-inside">
-        {questions.map((question, index) => (
+        {activeSubjectQuestions.map((question, index) => (
           <li
             key={index}
             className="mb-4 bg-white p-4 shadow rounded text-lg font-semibold"
